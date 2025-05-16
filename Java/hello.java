4803c2c174b4d0a87bd63aa2f48808d1ad945c7b
zerkjee <157850833+zerkjee@users.noreply.github.com>
2025-05-16 16:45:05 -0300

    java

--- Java/hello.java
+++ Java/hello.java
@@ -1,4 +1,4 @@
 public class hello{
 public static void main(string[] args){
-System.out.printIn("esse é o nosso primeiro código em java");}
+System.out.print("esse é o nosso primeiro código em java");}
 }
