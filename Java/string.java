4ad096388cf8e946993ac171ea15e47c315b936c
zerkjee <157850833+zerkjee@users.noreply.github.com>
2025-05-19 11:57:37 -0300

    Avanço

--- Java/string.java
+++ Java/string.java
@@ -1,4 +1,12 @@
 
 public class string {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    @Override
+    public String toString() {
+        return "string []";
+    }
 
 }
