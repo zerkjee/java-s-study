999185365bf1ebe403285369c95ff88a3bdbefb9
zerkjee <157850833+zerkjee@users.noreply.github.com>
2025-05-29 11:03:18 -0300

    Update string.java

--- Java/string.java
+++ Java/string.java
@@ -1,11 +1,2 @@
-
-public class string {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-
-    @Override
-    public String toString() {
-        return "string []";
-    }
- }
+public class string
+	public static void main (string[])
