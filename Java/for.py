b005a0e682209e2e56e1b896555579c04dd71d12
zerkjee <157850833+zerkjee@users.noreply.github.com>
2025-06-16 21:37:37 -0300

    inicio de tudo

--- Java/for.py
+++ Java/for.py
@@ -1,7 +1,1 @@
-def verifica_par(numero):
-        if numero % 2 == 0:
-                print('par')
-        else:
-                print('impar')
-verifica_par(7)
-verifica_par(10)
+numero = int(input("asd"))
