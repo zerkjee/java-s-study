4e03e024225fff34336e9e0e774085e542c77b38
zerkjee <157850833+zerkjee@users.noreply.github.com>
2025-06-16 21:27:12 -0300

    forss

--- Java/for.py
+++ Java/for.py
@@ -1,3 +1,7 @@
-frutas = ["Maçã", "Banana", "Melancia"]
-for x in frutas:
-    print(x)
+def verifica_par(numero):
+        if numero % 2 == 0:
+                print('par')
+        else:
+                print('impar')
+verifica_par(7)
+verifica_par(10)
--- Java/hello.java
+++
@@ -1,4 +0,0 @@
-public class hello{
-        public static void main(string[] args){
-            System.out.print("esse é o nosso primeiro código em java");}
-}
