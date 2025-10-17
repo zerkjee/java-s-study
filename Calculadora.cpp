a06eb3e2315fd1eb9f17a36e8a6092a54733e123
zerkjee <157850833+zerkjee@users.noreply.github.com>
2025-10-17 19:35:01 -0300

    Refatora calculadora e adiciona função de potência
    Refatora o código da calculadora para melhorar a organização e legibilidade, renomeando funções e corrigindo mensagens. Implementa funções exponencial e logarítmica para permitir o cálculo de potência sem usar bibliotecas externas. Ajusta o menu e corrige a operação de subtração.

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,50 +1,74 @@
 #include <cstdio>
-double meu_exp(double x) {
+
+double exponencial(double x) {
     double resultado = 1.0;
     double termo = 1.0;
     int N = 20;
-    for(int i = 1; i <= N; i++){
+    for (int i = 1; i <= N; i++) {
         termo = termo * (x / i);
         resultado += termo;
-
     }
     return resultado;
+}
 
-};    
-double meu_in(double x){
+double log(double x) {
     double resultado = 0.0;
     double y = x - 1.0;
-}
-    int main(){
-double a, b;
-int opcao;
-printf("1 - Adição\n");
-printf("2 - Subtração\n");
-printf("3 - Multiplicação\n");
-printf("4 - Divisão\n");
-printf("5 - Potencia\n");
-printf("6 - Seno\n");
-printf("7 - Cosseno\n");
-printf("0 - Sair\n");
-scanf("%d", &opcao);
-printf("Digite o Primeiro número: ");
-scanf("%lf", &a);
-printf("Digite o Segundo número: ");
-scanf("%lf", &b);
-if (opcao == 1 ){
-    printf("O resultado da sua Adição é :%.2lf\n", a + b);
-}
-if (opcao == 2){
-    printf("O resultado da sua Divisão é :%.2lf\n", a - b);
-}
-if (opcao == 3){
-    printf("O resultado da sua Multiplicação é :%.2lf\n", a * b);
-}
-if (opcao == 4){
-    if(b !=0) printf("O resultado da sua Divisão é:%.2lf\n", a / b );
-    else printf("Erro: divisão por zero!\n");
+    double termo = y;
+    int N = 20;
+
+    for (int i = 1; i <= N; i++) {
+        if (i > 1) termo *= y;
+        if (i % 2 == 0)
+            resultado -= termo / i;
+        else
+            resultado += termo / i;
+    }
+
+    return resultado;
 }
 
+double pow(double a, double b) {
+    return exponencial(b * log(a));
 }
 
+int main() {
+    double a, b;
+    int opcao;
 
+    printf("1 - Adição\n");
+    printf("2 - Subtração\n");
+    printf("3 - Multiplicação\n");
+    printf("4 - Divisão\n");
+    printf("5 - Potência\n");
+    printf("6 - Seno\n");
+    printf("7 - Cosseno\n");
+    printf("0 - Sair\n");
+    scanf("%d", &opcao);
+
+    printf("Digite o Primeiro número: ");
+    scanf("%lf", &a);
+    printf("Digite o Segundo número: ");
+    scanf("%lf", &b);
+
+    if (opcao == 1)
+        printf("O resultado da sua Adição é: %.2lf\n", a + b);
+
+    if (opcao == 2)
+        printf("O resultado da sua Subtração é: %.2lf\n", a - b);
+
+    if (opcao == 3)
+        printf("O resultado da sua Multiplicação é: %.2lf\n", a * b);
+
+    if (opcao == 4) {
+        if (b != 0)
+            printf("O resultado da sua Divisão é: %.2lf\n", a / b);
+        else
+            printf("Erro: divisão por zero!\n");
+    }
+
+    if (opcao == 5)
+        printf("O resultado da sua Potência é: %.5lf\n", pow(a, b));
+
+    return 0;
+}
