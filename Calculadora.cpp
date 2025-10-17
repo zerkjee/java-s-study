acbaefe4b32d880e620fd248a12ff8714ec1bee6
zerkjee <157850833+zerkjee@users.noreply.github.com>
2025-10-17 14:17:58 -0300

    Add exp and log function stubs; improve output messages
    Introduces stubs for custom exponential and logarithm functions. Refines calculator output messages for clarity and adds error handling for division by zero.

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,4 +1,20 @@
 #include <cstdio>
+double meu_exp(double x) {
+    double resultado = 1.0;
+    double termo = 1.0;
+    int N = 20;
+    for(int i = 1; i <= N; i++){
+        termo = termo * (x / i);
+        resultado += termo;
+
+    }
+    return resultado;
+
+};    
+double meu_in(double x){
+    double resultado = 0.0;
+    double y = x - 1.0;
+}
     int main(){
 double a, b;
 int opcao;
@@ -16,12 +32,18 @@ scanf("%lf", &a);
 printf("Digite o Segundo número: ");
 scanf("%lf", &b);
 if (opcao == 1 ){
-    printf("O Resultado;%2lf\n", a + b);
-
+    printf("O resultado da sua Adição é :%.2lf\n", a + b);
 }
-
-
-
+if (opcao == 2){
+    printf("O resultado da sua Divisão é :%.2lf\n", a - b);
+}
+if (opcao == 3){
+    printf("O resultado da sua Multiplicação é :%.2lf\n", a * b);
+}
+if (opcao == 4){
+    if(b !=0) printf("O resultado da sua Divisão é:%.2lf\n", a / b );
+    else printf("Erro: divisão por zero!\n");
+}
 
 }
 
