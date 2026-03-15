386551153861d18b2c08bec646659b4557e89dc3
pedro <pedropapa638@gmail.com>
2026-03-15 15:00:26 -0300

    nothing

--- java - study/src/etapa1_logica/calculadora.java
+++ java - study/src/etapa1_logica/calculadora.java
@@ -1,35 +1,54 @@
-package etapa1_logica;
 import java.util.Scanner;
 
-public class calculadora {
+public class Calculadora {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        boolean continuar = true;
 
-        System.out.println("Digite o primeiro número: ");
-        double n1 = sc.nextDouble();
+        while (continuar) {
 
-        System.out.println("Digite o segundo número: ");
-        double n2 = sc.nextDouble();
+            System.out.print("Digite o primeiro número: ");
+            double n1 = sc.nextDouble();
 
-        System.out.println("Digite a operação (+,-,*,/):");
-        String op = sc.next();
+            System.out.print("Digite o segundo número: ");
+            double n2 = sc.nextDouble();
 
-        double resultado;
-addsas
+            System.out.print("Escolha a operação (+ - * /): ");
+            String op = sc.next();
 
-        if(op.equals("+")) {
-            resultado = n1 + n2;
-        } else if(op.equals("-")) {
-            resultado = n1 - n2;
-        } else if(op.equals("*")) {
-            resultado = n1 * n2;
-        } else if(op.equals("/")) {
-            resultado = n1 / n2;
-        } else {
-            System.out.println("Operação invalida");
-        return;
+            double resultado = 0;
+
+            if (op.equals("+")) {
+                resultado = n1 + n2;
+
+            } else if (op.equals("-")) {
+                resultado = n1 - n2;
+
+            } else if (op.equals("*")) {
+                resultado = n1 * n2;
+
+            } else if (op.equals("/")) {
+
+                if (n2 == 0) {
+                    System.out.println("Erro: divisão por zero!");
+                    continue;
+                }
+
+                resultado = n1 / n2;
+
+            } else {
+                System.out.println("Operação inválida!");
+                continue;
+            }
+
+            System.out.println("Resultado: " + resultado);
+
+            System.out.print("Deseja fazer outro cálculo? (true/false): ");
+            continuar = sc.nextBoolean();
         }
-        System.out.println("Resultado: " + resultado);
+
+        System.out.println("Programa encerrado.");
     }
 }
